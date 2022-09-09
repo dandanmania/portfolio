1934fa57a1560c690c43a4f387bc7963bd9ebd16
dandanmania <danielkimus@gmail.com>
2022-09-09 16:36:58 -0700

    Add navbar styling

--- components/navbar.js
+++ components/navbar.js
@@ -1,15 +1,14 @@
-import Image from "next/image";
 import Link from "next/link";
 import userData from "../constants/data";
-
+import styles from "./navbar.module.css"
 
 export default function Navbar() {
     return (
-            <div style={{display: 'flex', flex: 'column', alignItems: 'center', padding: '32px 48px'}}>
+            <div className={styles.navbar}>
                 <Link href="/">
                     <a>
-                        <h1 style={{marginTop: 0, marginBottom: 0}}>{userData.name}</h1>
-                        <p style={{marginTop: 0}}>{userData.designation}</p>
+                        <h1 className={styles.name}>{userData.name}</h1>
+                        <p className={styles.designation}>{userData.designation}</p>
                     </a>
                 </Link>
                 <span style={{flex: '1 1 auto'}}></span>
@@ -21,12 +20,13 @@ export default function Navbar() {
                         height="32"
                         fill="currentColor"
                         viewBox="0 0 16 16"
+                        className={styles.svg}
                     >
                     <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
                     </svg>
                     </a>
                 </Link>
-                <span style={{width: '40px'}}></span>
+                <span className={styles.svgSplitter}></span>
                 <Link href="https://twitter.com/kimosterferson">
                     <a target="_blank">
                     <svg
