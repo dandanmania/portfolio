4c1c90956a9715bb0327e616042b7725c3ace127
dandanmania <danielkimus@gmail.com>
2022-10-05 11:13:22 -0700

    Update Phone

--- constants/data.js
+++ constants/data.js
@@ -3,7 +3,7 @@ const userData = {
     name: "Daniel Kim",
     designation: "Aspiring Web Developer",
     email: "danielkimus@gmail.com",
-    phone: "+1 6198026936",
+    phone: "+1 7148867642",
 }
 
 export default userData;
